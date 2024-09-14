45ea4d77c2c5999c1d780f39442f2eabe226d112
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-14 03:07:05 +0100

    Add "renderer"

---
+++ crates/walnut-browser/src/canvas.rs
@@ -0,0 +1,34 @@
+use std::mem;
+
+use skia_safe::{surfaces, Color, Data, EncodedImageFormat, Paint, PaintStyle, Path, Surface};
+
+pub struct Canvas {
+    pub surface: Surface,
+    pub paint: Paint,
+}
+
+impl Canvas {
+    pub fn new(width: i32, height: i32) -> Canvas {
+        let mut surface = surfaces::raster_n32_premul((width, height)).expect("surface");
+        let mut paint = Paint::default();
+        paint.set_color(Color::BLACK);
+        paint.set_anti_alias(true);
+        paint.set_stroke_width(1.0);
+        surface.canvas().clear(Color::WHITE);
+        Canvas { surface, paint }
+    }
+
+    #[inline]
+    pub fn save(&mut self) {
+        self.surface.canvas().save();
+    }
+
+    #[inline]
+    pub fn data(&mut self) -> Data {
+        let image = self.surface.image_snapshot();
+        let mut context = self.surface.direct_context();
+        image
+            .encode(context.as_mut(), EncodedImageFormat::PNG, None)
+            .unwrap()
+    }
+}
--- crates/walnut-browser/src/main.rs
+++ crates/walnut-browser/src/main.rs
@@ -1,3 +1,73 @@
+mod canvas;
+use canvas::Canvas;
+use skia_safe::{
+    typeface, Color, Font, FontMgr, FontStyle, Paint, PaintStyle as Style, Point, TextBlob,
+    Typeface,
+};
+
+use core::str;
+use std::fs::File;
+use std::io::Write;
+
+use skia_safe::utils::text_utils::Align;
+
+pub const H1_FONT_SIZE: f32 = 56.0;
+pub const NORMAL_FONT_SIZE: f32 = 36.0;
+
 fn main() {
-    println!("Hello, world!");
+    // Get + parse HTML
+    let html = reqwest::blocking::get("http://info.cern.ch/hypertext/WWW/TheProject.html")
+        .unwrap()
+        .text()
+        .unwrap();
+    let dom = tl::parse(&html, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+
+    // Setup canvas and paint
+    let mut canvas = Canvas::new(2560, 1280);
+    let skia_canvas = canvas.surface.canvas();
+    let paint = canvas.paint.clone();
+
+    // Setup fonts
+    let font_mgr = FontMgr::new();
+    let typeface = font_mgr
+        .match_family_style("Arial", FontStyle::normal())
+        .unwrap_or_else(|| {
+            font_mgr
+                .legacy_make_typeface(None, FontStyle::normal())
+                .unwrap()
+        });
+    let h1_font = Font::new(typeface.clone(), H1_FONT_SIZE);
+    let normal_font = Font::new(typeface, NORMAL_FONT_SIZE);
+
+    // Paint nodes
+    let mut y = 50.0;
+    for node in dom.nodes() {
+        let tag_name = node
+            .as_tag()
+            .map(|tag| str::from_utf8(tag.name().as_bytes()))
+            .unwrap_or(Ok("P"))
+            .unwrap();
+        match tag_name {
+            "H1" => {
+                let text = node.inner_text(parser);
+                skia_canvas.draw_str(text, (50.0, y), &h1_font, &paint);
+                y += 50.0;
+            }
+            "P" | "A" => {
+                let text = node.inner_text(parser);
+                if text == "The World Wide Web project" {
+                    dbg!(&text, &tag_name);
+                }
+                skia_canvas.draw_str(text, (50.0, y), &normal_font, &paint);
+                y += 50.0;
+            }
+            _ => {}
+        }
+    }
+
+    let d = canvas.data();
+    let mut file = File::create("test.png").unwrap();
+    let bytes = d.as_bytes();
+    file.write_all(bytes).unwrap();
 }
